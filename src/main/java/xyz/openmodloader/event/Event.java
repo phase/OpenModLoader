7e4b5771b460d9f52aa6438e1edf3acf6b957e52
Tyler Hancock <darklime@live.ca>
2016-06-23 08:03:09 -0600

    Added documentation for Event

--- src/main/java/xyz/openmodloader/event/Event.java
+++ src/main/java/xyz/openmodloader/event/Event.java
@@ -1,12 +1,27 @@
 package xyz.openmodloader.event;
 
 public class Event {
+
+    /**
+     * Whether or not the event has been canceled.
+     */
     private boolean canceled;
 
+    /**
+     * Checks if the event can be canceled.
+     * 
+     * @return Whether or not the event can be canceled.
+     */
     public boolean isCancelable() {
         return false;
     }
 
+    /**
+     * Updated the canceled status of the event. True means canceled, false
+     * means not canceled.
+     * 
+     * @param canceled The new canceled status for the event.
+     */
     public void setCanceled(boolean canceled) {
         if (!this.isCancelable()) {
             throw new RuntimeException("Cannot cancel event " + this);
@@ -14,6 +29,11 @@ public class Event {
         this.canceled = canceled;
     }
 
+    /**
+     * Checks if the event has been canceled.
+     * 
+     * @return Whether or not the event has been canceled.
+     */
     public boolean isCanceled() {
         return canceled;
     }
