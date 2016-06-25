7734cc502b1aa500a349bffda39235c25433f1bc
CoolSquid <mail@coolsquid.me>
2016-06-25 19:52:38 +0200

    Javadocs

--- src/main/java/xyz/openmodloader/config/Config.java
+++ src/main/java/xyz/openmodloader/config/Config.java
@@ -12,20 +12,42 @@ import com.typesafe.config.ConfigFactory;
 import com.typesafe.config.ConfigRenderOptions;
 import com.typesafe.config.ConfigValueFactory;
 
+/**
+ * The base config class.
+ * Example:
+ * <code>
+ * <br>Config config = new Config("example.conf");
+ * <br>Config category1 = config.getConfig("category1", "Enables and disables blocks");
+ * <br>if (category1.getBoolean("block1", true, "Enables block1"))
+ * <br>    enableBlock1();
+ * </code>
+ */
 public class Config {
-    private static final File CONFIG_DIR = new File("./config");
 
-    private final int initialHash;
-    private final File file;
-    private final Config parent;
-    private final String path;
-    private com.typesafe.config.Config config;
+    /**
+     * The config directory.
+     */
+    public static final File CONFIG_DIR = new File("./config");
 
+    /**
+     * Creates a new config within the config directory, using the filename provided.
+     * 
+     * @param file The name of the config file. Must be a .conf file.
+     */
     public Config(String file) {
         this(new File(CONFIG_DIR, file));
     }
 
+    /**
+     * Creates a new config using the file provided.
+     *
+     * @param file The file to use for the config.
+     * Must be a .conf file located within the config directory.
+     */
     public Config(File file) {
+        if (!file.getName().endsWith(".conf")) {
+            throw new IllegalArgumentException("Config file has to be a .conf file");
+        }
         boolean flag = false;
         File parent = file.getParentFile();
         while (parent != null && !flag) {
@@ -39,150 +61,294 @@ public class Config {
         this.file = file;
         this.config = ConfigFactory.parseFile(file);
         this.initialHash = System.identityHashCode(config);
-        this.path = null;
+        this.name = null;
         this.parent = null;
     }
 
-    private Config(String path, com.typesafe.config.Config config, Config parent) {
-        this.file = null;
-        this.config = config;
-        this.initialHash = System.identityHashCode(config);
-        this.parent = parent;
-        this.path = path;
-    }
-
-    public boolean getBoolean(String path, boolean defaultValue, String comment) {
-        if (!config.hasPath(path)) {
-            config = config.withValue(path, ConfigValueFactory.fromAnyRef(defaultValue, comment));
+    /**
+     * Gets a boolean from the config.
+     * If the property doesn't exist,
+     * a new one is added, with the provided
+     * default value.
+     *
+     * @param name The name of the property
+     * @param defaultValue The default value of the property.
+     * @param comment The comment to set for the property.
+     * @return The requested boolean property, or the defaultValue if it doesn't exist.
+     */
+    public boolean getBoolean(String name, boolean defaultValue, String comment) {
+        if (!config.hasPath(name)) {
+            config = config.withValue(name, ConfigValueFactory.fromAnyRef(defaultValue, comment));
             if (parent != null) {
                 parent.update(this);
             }
         }
-        return config.getBoolean(path);
+        return config.getBoolean(name);
     }
 
-    public int getInt(String path, int defaultValue, String comment) {
-        if (!config.hasPath(path)) {
-            config = config.withValue(path, ConfigValueFactory.fromAnyRef(defaultValue, comment));
+    /**
+     * Gets an integer from the config.
+     * If the property doesn't exist,
+     * a new one is added, with the provided
+     * default value.
+     *
+     * @param name The name of the property
+     * @param defaultValue The default value of the property.
+     * @param comment The comment to set for the property.
+     * @return The requested integer property, or the defaultValue if it doesn't exist.
+     */
+    public int getInt(String name, int defaultValue, String comment) {
+        if (!config.hasPath(name)) {
+            config = config.withValue(name, ConfigValueFactory.fromAnyRef(defaultValue, comment));
             if (parent != null) {
                 parent.update(this);
             }
         }
-        return config.getInt(path);
+        return config.getInt(name);
     }
 
-    public long getLong(String path, long defaultValue, String comment) {
-        if (!config.hasPath(path)) {
-            config = config.withValue(path, ConfigValueFactory.fromAnyRef(defaultValue, comment));
+    /**
+     * Gets a long from the config.
+     * If the property doesn't exist,
+     * a new one is added, with the provided
+     * default value.
+     *
+     * @param name The name of the property
+     * @param defaultValue The default value of the property.
+     * @param comment The comment to set for the property.
+     * @return The requested long property, or the defaultValue if it doesn't exist.
+     */
+    public long getLong(String name, long defaultValue, String comment) {
+        if (!config.hasPath(name)) {
+            config = config.withValue(name, ConfigValueFactory.fromAnyRef(defaultValue, comment));
             if (parent != null) {
                 parent.update(this);
             }
         }
-        return config.getLong(path);
+        return config.getLong(name);
     }
 
-    public double getDouble(String path, double defaultValue, String comment) {
-        if (!config.hasPath(path)) {
-            config = config.withValue(path, ConfigValueFactory.fromAnyRef(defaultValue, comment));
+    /**
+     * Gets a double from the config.
+     * If the property doesn't exist,
+     * a new one is added, with the provided
+     * default value.
+     *
+     * @param name The name of the property
+     * @param defaultValue The default value of the property.
+     * @param comment The comment to set for the property.
+     * @return The requested double property, or the defaultValue if it doesn't exist.
+     */
+    public double getDouble(String name, double defaultValue, String comment) {
+        if (!config.hasPath(name)) {
+            config = config.withValue(name, ConfigValueFactory.fromAnyRef(defaultValue, comment));
             if (parent != null) {
                 parent.update(this);
             }
         }
-        return config.getDouble(path);
+        return config.getDouble(name);
     }
 
-    public String getString(String path, String defaultValue, String comment) {
-        if (!config.hasPath(path)) {
-            config = config.withValue(path, ConfigValueFactory.fromAnyRef(defaultValue, comment));
+    /**
+     * Gets a string from the config.
+     * If the property doesn't exist,
+     * a new one is added, with the provided
+     * default value.
+     *
+     * @param name The name of the property
+     * @param defaultValue The default value of the property.
+     * @param comment The comment to set for the property.
+     * @return The requested string property, or the defaultValue if it doesn't exist.
+     */
+    public String getString(String name, String defaultValue, String comment) {
+        if (!config.hasPath(name)) {
+            config = config.withValue(name, ConfigValueFactory.fromAnyRef(defaultValue, comment));
             if (parent != null) {
                 parent.update(this);
             }
         }
-        return config.getString(path);
+        return config.getString(name);
     }
 
-    public Config getConfig(String path, String comment) {
-        if (!config.hasPath(path)) {
-            config = config.withValue(path, ConfigValueFactory.fromMap(Collections.emptyMap(), comment));
+    /**
+     * Gets a subconfig from the config.
+     * If the property doesn't exist,
+     * a new one is added, with the provided
+     * default value.
+     *
+     * @param name The name of the property
+     * @param defaultValue The default value of the property.
+     * @param comment The comment to set for the property.
+     * @return The requested subconfig property, or the defaultValue if it doesn't exist.
+     */
+    public Config getConfig(String name, String comment) {
+        if (!config.hasPath(name)) {
+            config = config.withValue(name, ConfigValueFactory.fromMap(Collections.emptyMap(), comment));
             if (parent != null) {
                 parent.update(this);
             }
         }
-        return new Config(path, config.getConfig(path), this);
+        return new Config(name, config.getConfig(name), this);
     }
 
-    public List<Boolean> getBooleanList(String path, List<Boolean> defaultValue, String comment) {
-        if (!config.hasPath(path)) {
-            config = config.withValue(path, ConfigValueFactory.fromAnyRef(defaultValue, comment));
+    /**
+     * Gets a list of booleans from the config.
+     * If the property doesn't exist,
+     * a new one is added, with the provided
+     * default value.
+     *
+     * @param name The name of the property
+     * @param defaultValue The default value of the property.
+     * @param comment The comment to set for the property.
+     * @return The requested boolean list property, or the defaultValue if it doesn't exist.
+     */
+    public List<Boolean> getBooleanList(String name, List<Boolean> defaultValue, String comment) {
+        if (!config.hasPath(name)) {
+            config = config.withValue(name, ConfigValueFactory.fromAnyRef(defaultValue, comment));
             if (parent != null) {
                 parent.update(this);
             }
         }
-        return config.getBooleanList(path);
+        return config.getBooleanList(name);
     }
 
-    public List<Integer> getIntList(String path, List<Integer> defaultValue, String comment) {
-        if (!config.hasPath(path)) {
-            config = config.withValue(path, ConfigValueFactory.fromAnyRef(defaultValue, comment));
+    /**
+     * Gets a list of integers from the config.
+     * If the property doesn't exist,
+     * a new one is added, with the provided
+     * default value.
+     *
+     * @param name The name of the property
+     * @param defaultValue The default value of the property.
+     * @param comment The comment to set for the property.
+     * @return The requested integer list property, or the defaultValue if it doesn't exist.
+     */
+    public List<Integer> getIntList(String name, List<Integer> defaultValue, String comment) {
+        if (!config.hasPath(name)) {
+            config = config.withValue(name, ConfigValueFactory.fromAnyRef(defaultValue, comment));
             if (parent != null) {
                 parent.update(this);
             }
         }
-        return config.getIntList(path);
+        return config.getIntList(name);
     }
 
-    public List<Long> getLongList(String path, List<Long> defaultValue, String comment) {
-        if (!config.hasPath(path)) {
-            config = config.withValue(path, ConfigValueFactory.fromAnyRef(defaultValue, comment));
+    /**
+     * Gets a list of longs from the config.
+     * If the property doesn't exist,
+     * a new one is added, with the provided
+     * default value.
+     *
+     * @param name The name of the property
+     * @param defaultValue The default value of the property.
+     * @param comment The comment to set for the property.
+     * @return The requested long list property, or the defaultValue if it doesn't exist.
+     */
+    public List<Long> getLongList(String name, List<Long> defaultValue, String comment) {
+        if (!config.hasPath(name)) {
+            config = config.withValue(name, ConfigValueFactory.fromAnyRef(defaultValue, comment));
             if (parent != null) {
                 parent.update(this);
             }
         }
-        return config.getLongList(path);
+        return config.getLongList(name);
     }
 
-    public List<Double> getDoubleList(String path, List<Double> defaultValue, String comment) {
-        if (!config.hasPath(path)) {
-            config = config.withValue(path, ConfigValueFactory.fromAnyRef(defaultValue, comment));
+    /**
+     * Gets a list of doubles from the config.
+     * If the property doesn't exist,
+     * a new one is added, with the provided
+     * default value.
+     *
+     * @param name The name of the property
+     * @param defaultValue The default value of the property.
+     * @param comment The comment to set for the property.
+     * @return The requested double list property, or the defaultValue if it doesn't exist.
+     */
+    public List<Double> getDoubleList(String name, List<Double> defaultValue, String comment) {
+        if (!config.hasPath(name)) {
+            config = config.withValue(name, ConfigValueFactory.fromAnyRef(defaultValue, comment));
             if (parent != null) {
                 parent.update(this);
             }
         }
-        return config.getDoubleList(path);
+        return config.getDoubleList(name);
     }
 
-    public List<String> getStringList(String path, List<String> defaultValue, String comment) {
-        if (!config.hasPath(path)) {
-            config = config.withValue(path, ConfigValueFactory.fromAnyRef(defaultValue, comment));
+    /**
+     * Gets a list of strings from the config.
+     * If the property doesn't exist,
+     * a new one is added, with the provided
+     * default value.
+     *
+     * @param name The name of the property
+     * @param defaultValue The default value of the property.
+     * @param comment The comment to set for the property.
+     * @return The requested string list property, or the defaultValue if it doesn't exist.
+     */
+    public List<String> getStringList(String name, List<String> defaultValue, String comment) {
+        if (!config.hasPath(name)) {
+            config = config.withValue(name, ConfigValueFactory.fromAnyRef(defaultValue, comment));
             if (parent != null) {
                 parent.update(this);
             }
         }
-        return config.getStringList(path);
+        return config.getStringList(name);
     }
 
-    public List<Config> getConfigList(String path, String comment) {
-        if (!config.hasPath(path)) {
-            config = config.withValue(path, ConfigValueFactory.fromIterable(Collections.emptyList()));
+    /**
+     * Gets a list of subconfigs from the config.
+     * If the property doesn't exist,
+     * a new one is added, with the provided
+     * default value.
+     *
+     * @param name The name of the property
+     * @param defaultValue The default value of the property.
+     * @param comment The comment to set for the property.
+     * @return The requested subconfig list property, or the defaultValue if it doesn't exist.
+     */
+    public List<Config> getConfigList(String name, String comment) {
+        if (!config.hasPath(name)) {
+            config = config.withValue(name, ConfigValueFactory.fromIterable(Collections.emptyList()));
             if (parent != null) {
                 parent.update(this);
             }
         }
-        return Lists.transform(config.getConfigList(path), (c) -> new Config(path, c, this));
+        return Lists.transform(config.getConfigList(name), (c) -> new Config(name, c, this));
     }
 
-    public boolean hasPath(String path) {
-        return config.hasPath(path);
+    /**
+     * Checks whether a property with
+     * the specified name exists.
+     *
+     * @param name The name of the property.
+     * @return true, if the property exists.
+     */
+    public boolean hasProperty(String name) {
+        return config.hasPath(name);
     }
 
+    /**
+     * Checks if the config is empty.
+     *
+     * @return true, if the config is empty
+     */
     public boolean isEmpty() {
         return config.isEmpty();
     }
 
+    /**
+     * Checks if the config has changed since it was loaded.
+     *
+     * @return true, if the config has changed since it was loaded.
+     */
     public boolean hasChanged() {
         return System.identityHashCode(config) != initialHash;
     }
 
+    /**
+     * Saves the config to disc.
+     */
     public void save() {
         if (file == null) {
             throw new UnsupportedOperationException("Only root configs can be saved!");
@@ -196,8 +362,26 @@ public class Config {
         }
     }
 
+    /**
+     * INTERNAL.
+     */
+
+    private final int initialHash;
+    private final File file;
+    private final Config parent;
+    private final String name;
+    private com.typesafe.config.Config config;
+
+    private Config(String name, com.typesafe.config.Config config, Config parent) {
+        this.file = null;
+        this.config = config;
+        this.initialHash = System.identityHashCode(config);
+        this.parent = parent;
+        this.name = name;
+    }
+
     private void update(Config child) {
-        this.config = this.config.withValue(child.path, child.config.root());
+        this.config = this.config.withValue(child.name, child.config.root());
         if (parent != null) {
             parent.update(this);
         }
--- src/main/java/xyz/openmodloader/launcher/strippable/Strippable.java
+++ src/main/java/xyz/openmodloader/launcher/strippable/Strippable.java
@@ -6,29 +6,66 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Applied to a method, constructor, field or class
+ * to remove it if the specified conditions aren't met.
+ */
 @Retention(value = RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.FIELD, ElementType.METHOD, ElementType.CONSTRUCTOR})
 public @interface Strippable {
+
+    /**
+     * Strips the element unless the runtime side matches the specified one.
+     */
     Side side() default Side.UNIVERSAL;
 
+    /**
+     * Strips the element unless the runtime environment type matches the specified one.
+     */
     Environment environment() default Environment.UNIVERSAL;
 
+    /**
+     * Strips the element unless the specified mods are loaded.
+     */
     String[] mods() default {};
 
+    /**
+     * Strips the element unless the specified classes exist.
+     */
     String[] classes() default {};
 
+    /**
+     * Removes the specified interfaces unless the
+     * specified conditions are met.
+     */
     @Retention(value = RetentionPolicy.RUNTIME)
     @Target(ElementType.TYPE)
     @Repeatable(value = InterfaceContainer.class)
     public static @interface Interface {
+        
+        /**
+         * The interfaces to strip.
+         */
         String[] interfaces();
         
+        /**
+         * Strips the interfaces unless the runtime side matches the specified one.
+         */
         Side side() default Side.UNIVERSAL;
 
+        /**
+         * Strips the interfaces unless the runtime environment type matches the specified one.
+         */
         Environment environment() default Environment.UNIVERSAL;
 
+        /**
+         * Strips the interfaces unless the specified mods are loaded.
+         */
         String[] mods() default {};
 
+        /**
+         * Strips the interfaces unless the specified classes exist.
+         */
         String[] classes() default {};
     }
 }
