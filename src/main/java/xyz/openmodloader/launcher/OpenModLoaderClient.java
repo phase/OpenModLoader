3e9e2dd716984e6ab78bdf6bc206e7771c287cc1
iLexiconn <me@ilexiconn.net>
2016-06-25 12:29:59 +0200

    Better argument parsing

--- src/main/java/xyz/openmodloader/launcher/OpenModLoaderClient.java
+++ src/main/java/xyz/openmodloader/launcher/OpenModLoaderClient.java
@@ -1,7 +1,5 @@
 package xyz.openmodloader.launcher;
 
-import com.google.common.base.Strings;
-import com.google.common.base.Throwables;
 import com.google.gson.GsonBuilder;
 import com.mojang.authlib.Agent;
 import com.mojang.authlib.exceptions.AuthenticationException;
@@ -9,24 +7,23 @@ import com.mojang.authlib.properties.PropertyMap;
 import com.mojang.authlib.yggdrasil.YggdrasilAuthenticationService;
 import com.mojang.authlib.yggdrasil.YggdrasilUserAuthentication;
 import net.minecraft.launchwrapper.Launch;
+import xyz.openmodloader.event.strippable.Side;
 
 import java.io.File;
 import java.net.Proxy;
-import java.util.*;
-
-import xyz.openmodloader.OpenModLoader;
-import xyz.openmodloader.event.strippable.Side;
+import java.util.Arrays;
 
 public class OpenModLoaderClient {
     public static void main(String[] args) {
         System.setProperty("org.lwjgl.librarypath", new File("../.gradle/minecraft/natives/").getAbsolutePath());
         OMLSideTransformer.SIDE = Side.CLIENT;
-        Map<String, String> argMap = parseArgs(args);
-        if (argMap.containsKey("password")) {
+        LaunchArguments arguments = new LaunchArguments(args);
+
+        if (arguments.containsArgument("password")) {
             YggdrasilUserAuthentication auth = (YggdrasilUserAuthentication) (new YggdrasilAuthenticationService(Proxy.NO_PROXY, "1")).createUserAuthentication(Agent.MINECRAFT);
-            auth.setUsername(argMap.get("username"));
-            auth.setPassword(argMap.get("password"));
-            argMap.remove("password");
+            auth.setUsername(arguments.getArgument("username"));
+            auth.setPassword(arguments.getArgument("password"));
+            arguments.removeArgument("password");
 
             try {
                 auth.logIn();
@@ -35,39 +32,19 @@ public class OpenModLoaderClient {
                 return;
             }
 
-            argMap.put("accessToken", auth.getAuthenticatedToken());
-            argMap.put("uuid", auth.getSelectedProfile().getId().toString().replace("-", ""));
-            argMap.put("username", auth.getSelectedProfile().getName());
-            argMap.put("userType", auth.getUserType().getName());
-            argMap.put("userProperties", new GsonBuilder().registerTypeAdapter(PropertyMap.class, new PropertyMap.Serializer()).create().toJson(auth.getUserProperties()));
+            arguments.addArgument("accessToken", auth.getAuthenticatedToken());
+            arguments.addArgument("uuid", auth.getSelectedProfile().getId().toString().replace("-", ""));
+            arguments.addArgument("username", auth.getSelectedProfile().getName());
+            arguments.addArgument("userType", auth.getUserType().getName());
+            arguments.addArgument("userProperties", new GsonBuilder().registerTypeAdapter(PropertyMap.class, new PropertyMap.Serializer()).create().toJson(auth.getUserProperties()));
         }
-        Launch.main(getArgs(argMap));
-    }
 
-    private static Map<String, String> parseArgs(String[] args) {
-        Map<String, String> argMap = new HashMap<>();
-        for (int i = 0; i < args.length; i++) {
-            if (args[i].startsWith("--")) {
-                argMap.put(args[i].substring(2, args[i].length()), args[i + 1]);
-            }
-        }
-        return argMap;
-    }
+        arguments.addArgument("version", "1.10");
+        arguments.addArgument("assetIndex", "1.10");
+        arguments.addArgument("accessToken", "OpenModLoader");
+        arguments.addArgument("tweakClass", "xyz.openmodloader.launcher.OMLTweaker");
 
-    private static String[] getArgs(Map<String, String> argMap) {
-        argMap.put("version", "1.10");
-        argMap.put("assetIndex", "1.10");
-        argMap.put("accessToken", "OpenModLoader");
-        argMap.put("tweakClass", "xyz.openmodloader.launcher.OMLTweaker");
-
-        List<String> list = new ArrayList<>();
-        for (Map.Entry<String, String> entry : argMap.entrySet()) {
-            String x = entry.getValue();
-            if (!Strings.isNullOrEmpty(x)) {
-                list.add("--" + entry.getKey());
-                list.add(x);
-            }
-        }
-        return list.toArray(new String[list.size()]);
+        System.out.println(Arrays.toString(arguments.getArguments()));
+        Launch.main(arguments.getArguments());
     }
 }
