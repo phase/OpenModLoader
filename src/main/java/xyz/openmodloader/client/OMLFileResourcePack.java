91ce4a6064972509b223d74a15f94e468094e2d5
Shadowfacts <me@shadowfacts.net>
2016-06-26 15:09:05 -0400

    Fix pack.mcmeta for mod resource packs

--- src/main/java/xyz/openmodloader/client/OMLFileResourcePack.java
+++ src/main/java/xyz/openmodloader/client/OMLFileResourcePack.java
@@ -5,7 +5,9 @@ import net.minecraft.client.resources.FileResourcePack;
 import xyz.openmodloader.modloader.ModContainer;
 
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class OMLFileResourcePack extends FileResourcePack {
 	private ModContainer mod;
@@ -13,6 +15,25 @@ public class OMLFileResourcePack extends FileResourcePack {
 	public OMLFileResourcePack(ModContainer mod) {
 		super(mod.getModFile());
 		this.mod = mod;
+	}
+
+	@Override
+	protected InputStream getInputStreamByName(String name) throws IOException {
+		try {
+			return super.getInputStreamByName(name);
+		} catch (IOException e) {
+			if ("pack.mcmeta".equals(name)) {
+				return new ByteArrayInputStream(
+							("{\n" +
+							"\"pack\": {\n" +
+							"\"description\": \"OML dummy resource pack for " + mod.getModID() + "\",\n" +
+							"\"pack_format\": 1\n" +
+							"}\n" +
+							"}").getBytes());
+			} else {
+				throw e;
+			}
+		}
 	}
 
 	@Override
--- src/main/java/xyz/openmodloader/client/OMLFolderResourcePack.java
+++ src/main/java/xyz/openmodloader/client/OMLFolderResourcePack.java
@@ -5,7 +5,9 @@ import net.minecraft.client.resources.FolderResourcePack;
 import xyz.openmodloader.modloader.ModContainer;
 
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class OMLFolderResourcePack extends FolderResourcePack {
 	private ModContainer mod;
@@ -13,6 +15,25 @@ public class OMLFolderResourcePack extends FolderResourcePack {
 	public OMLFolderResourcePack(ModContainer mod) {
 		super(mod.getModFile());
 		this.mod = mod;
+	}
+
+	@Override
+	protected InputStream getInputStreamByName(String name) throws IOException {
+		try {
+			return super.getInputStreamByName(name);
+		} catch (IOException e) {
+			if ("pack.mcmeta".equals(name)) {
+				return new ByteArrayInputStream(
+							("{\n" +
+							"\"pack\": {\n" +
+							"\"description\": \"OML dummy resource pack for " + mod.getModID() + "\",\n" +
+							"\"pack_format\": 1\n" +
+							"}\n" +
+							"}").getBytes());
+			} else {
+				throw e;
+			}
+		}
 	}
 
 	@Override
