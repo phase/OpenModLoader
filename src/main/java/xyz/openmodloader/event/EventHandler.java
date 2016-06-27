8dec6b1097a7e9382ca8a8fc4c2af05c0983998a
Shadowfacts <me@shadowfacts.net>
2016-06-27 13:59:44 -0400

    Add JavaDoc to EventHandler

--- src/main/java/xyz/openmodloader/event/EventHandler.java
+++ src/main/java/xyz/openmodloader/event/EventHandler.java
@@ -5,6 +5,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation marking a method as an event handler.
+ * Must be non-static.
+ * The method must take 1 parameter, a class extending {@link Event}.
+ * @see EventBus#register(Object) 
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface EventHandler {
