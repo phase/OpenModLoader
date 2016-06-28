ec8e97fd9a5382eeea6df949e63240d48b7b1ff4
Shadowfacts <me@shadowfacts.net>
2016-06-28 16:39:03 -0400

    Fix ReflectionHelper.getValue and add ReflectionHelper.setValue for mod's use

--- src/main/java/xyz/openmodloader/util/ReflectionHelper.java
+++ src/main/java/xyz/openmodloader/util/ReflectionHelper.java
@@ -7,9 +7,22 @@ public class ReflectionHelper {
 
 	public static <T, V> V getValue(Class<T> clazz, T object, String... names) throws ReflectiveOperationException {
 		for (String name : names) {
-			Field f = clazz.getDeclaredField(name);
-			f.setAccessible(true);
-			return (V)f.get(object);
+			try {
+				Field f = clazz.getDeclaredField(name);
+				f.setAccessible(true);
+				return (V)f.get(object);
+			} catch (NoSuchFieldException ignored) {}
+		}
+		throw new IllegalArgumentException(String.format("Class %s does not contain any of the fields %s", clazz.getName(), Arrays.toString(names)));
+	}
+
+	public static <T> void setValue(Class<T> clazz, T object, Object value, String... names) throws ReflectiveOperationException {
+		for (String name : names) {
+			try {
+				Field f = clazz.getDeclaredField(name);
+				f.setAccessible(true);
+				f.set(object, value);
+			} catch (NoSuchFieldException ignored) {}
 		}
 		throw new IllegalArgumentException(String.format("Class %s does not contain any of the fields %s", clazz.getName(), Arrays.toString(names)));
 	}
